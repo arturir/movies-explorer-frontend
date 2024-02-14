961b070e8ea66391b88ab53042c951d9d4733f60
arturir <artifakt666@yandex.ru>
2024-02-14 14:27:02 +0300

    bugfix: исправить ошибку в регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Form from "../Form/Form";
 import FormInputName from "../Form/FormInputName/FormInputName";
 import FormInputEmail from "../Form/FormInputEmail/FormInputEmail";
@@ -6,10 +6,20 @@ import FormInputPassword from "../Form/FormInputPassword/FormInputPassword";
 import FormSubmitButton from "../Form/FormSubmitButton/FormSubmitButton";
 import useFormWithValidation from "../../hooks/useFormWithValidation/useFormWithValidation";
 import mainApi from "../../utils/MainApi";
+import { useContext, useEffect } from "react";
+import { CurrentAppContext } from "../../contexts/CurrentAppContext";
 
 export default function Register ({handleInfoTooltip, handleAuthorize}) {
 
-    const formWithValidation = useFormWithValidation();
+    const formWithValidation = useFormWithValidation(),
+          {loggedIn, email} = useContext(CurrentAppContext),
+          navigate = useNavigate();
+
+    useEffect(() => {
+        if(loggedIn && email) { 
+            navigate("/", {relative: "path"})
+        }
+    },[loggedIn, email])
 
     function changeForm (event) {
         event.preventDefault();
